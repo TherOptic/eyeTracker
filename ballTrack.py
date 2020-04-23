c050cf17f6e617042cd5c9636fd39018839c3e59
revan <revan.macqueen1@gmail.com>
2020-04-22 19:26:50 -0600

    cleaned up pupil detection

--- ballTrack.py
+++ ballTrack.py
@@ -5,9 +5,9 @@ import random
 pygame.font.init() # you have to call this at the start, 
                 # if you want to use this module.
 
-textSize = 50
+textSize = 130
 comicSans = pygame.font.SysFont('Comic Sans MS', textSize )   
-
+clock = pygame.time.Clock()
 def main():
 
 
@@ -20,7 +20,7 @@ def main():
 def create_window():
     # Open a window on the display and rseturn its Surface
     title = 'Ball Track'
-    size = (1000, 664)
+    size =(1920, 1080)
     pygame.init()
     surface = pygame.display.set_mode(size, 0, 0)
     pygame.display.set_caption(title)
@@ -33,7 +33,7 @@ class Background(pygame.sprite.Sprite):
     def __init__(self, image_file, location):
         pygame.sprite.Sprite.__init__(self)  #call Sprite initializer
         self.image = pygame.image.load(image_file)
-        self.image = pygame.transform.scale(self.image, (1000, 664))
+        self.image = pygame.transform.scale(self.image,(1920, 1080))
         self.rect = self.image.get_rect()
         self.rect.left, self.rect.top = location
 
@@ -51,11 +51,12 @@ class Game:
         self.close_clicked = False
         self.continue_game = True
         ball_location = [self.surface.get_width()//2, self.surface.get_height()//2]
-        ball_size = 40
-        ball_velocity = [2,2]
+        ball_size = 150
+        ball_velocity = [10,10]
         self.updateBallFreq = 3
         self.ball = Ball(self.surface, ball_location, ball_size , self.fg_color, ball_velocity )
 
+        
         self.startTime = time.perf_counter()
 
     def play(self):
@@ -63,11 +64,13 @@ class Game:
       # - self is the Game that should be continued or not.
  
         self.draw()
+        self.surface.blit(self.BackGround.image, self.BackGround.rect)
         while not self.close_clicked: # until player clicks close box
             self.update()
             self.handle_event()
             self.draw()
-            time.sleep(self.pause_time) # set game velocity by pausing
+            clock.tick(60)
+            #time.sleep(self.pause_time) # set game velocity by pausing
 
     def draw(self):
         # Draw all game objects.
@@ -75,7 +78,8 @@ class Game:
         
         #self.surface.fill(self.bg_color)
         #self.surface.fill([255, 255, 255])
-        self.surface.blit(self.BackGround.image, self.BackGround.rect)
+       # 
+        self.surface.blit(self.BackGround.image, [self.ball.center[0]-50, self.ball.center[1]-50], pygame.Rect(self.ball.center[0]-50, self.ball.center[1]-50, 300, 300))
         self.ball.draw()
         pygame.display.update() #do not erase
 
@@ -106,7 +110,7 @@ class Ball:
       self.velocity = velocity
       self.image = pygame.image.load("ball.png")
 
-      self.image_size = (80, 80)
+      self.image_size = (150, 150)
       self.image = pygame.transform.scale(self.image, self.image_size)
       self.number = random.randint(0,10)
 
@@ -118,9 +122,10 @@ class Ball:
       #draws ball
     
       #pygame.draw.circle(self.surface,self.color,self.center,self.radius)
-      self.surface.blit(self.image, (self.center[0] - self.image_size[0]//2, self.center[1] -  self.image_size[1]//2))
+      #self.surface.blit(self.image, (self.center[0] - self.image_size[0]//2, self.center[1] -  self.image_size[1]//2))
+      self.surface.blit(self.image, (self.center[0], self.center[1]))
       textsurface = comicSans.render(str(self.number), False, (255, 255, 255))
-      self.surface.blit(textsurface, (self.center[0] - textSize // 2 + 5, self.center[1] - textSize // 2 - 10) )
+      self.surface.blit(textsurface, (self.center[0] + 50, self.center[1] + 30) )
    
    def move(self):
       #moves ball
--- pupilDetection.py
+++ pupilDetection.py
@@ -1,6 +1,18 @@
+'''
+Code demonstrating eyetracking using OpenCV
+
+To run: python3 pupilDetection.py
+
+Press q to quit  
+
+Some code modified from:
+https://medium.com/@stepanfilonov/tracking-your-eyes-with-python-3952e66194a6
+https://pysource.com/2019/01/04/eye-motion-tracking-opencv-with-python/
+'''
+
 import cv2
 import numpy as np
-import dlib
+import dlib, time, random
 
 detector_params = cv2.SimpleBlobDetector_Params()
 detector_params.filterByArea = True
@@ -13,17 +25,15 @@ predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
 
 
 def blob_process(img, threshold, side):
-    gray_frame = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    '''
+    Returns keyspoints where the pupils are
+    '''
+    gray_frame = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)    
     _, img = cv2.threshold(gray_frame, threshold, 255, cv2.THRESH_BINARY)    
     img = cv2.erode(img, None, iterations=1)
     img = cv2.dilate(img, None, iterations=1)
     # img = cv2.medianBlur(img, 5)
     keypoints = blob_detector.detect(img)
-
-    if side == "l":
-        cv2.imshow("left eye", img)
-    else:
-        cv2.imshow("right eye", img)
     
     return keypoints
 
@@ -31,71 +41,109 @@ def blob_process(img, threshold, side):
 def midpoint(p1 ,p2):
     return int((p1.x + p2.x)/2), int((p1.y + p2.y)/2)
 
+
 def nothing(x):
     pass
 
+
 def main():
     cap = cv2.VideoCapture(0)
-    cv2.namedWindow('frame')
+    cv2.namedWindow('frame', 0)
+
+    side = True
+
     cv2.createTrackbar('Left threshold', 'frame', 0, 255, nothing)
     cv2.createTrackbar('Right threshold', 'frame', 0, 255, nothing)
+    start = time.time()
+
+    cv2.resizeWindow('frame', 1080,1920)
+    dotx = random.randint(0, 500)
+    doty = random.randint(0, 500)
+    oldcoordleft = False
+    oldcoordright = False
     while True:
+
         _, frame = cap.read()
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         faces = detector(gray)
+
 
         for face in faces:
             x, y = face.left(), face.top()
             x1, y1 = face.right(), face.bottom()
             landmarks = predictor(gray, face)
 
-            # left eye on screen
+            # Process Left eye
             left_point = (landmarks.part(36).x, landmarks.part(36).y)
             right_point = (landmarks.part(39).x, landmarks.part(39).y)
 
             center_top = midpoint(landmarks.part(37), landmarks.part(38))
             center_bottom = midpoint(landmarks.part(41), landmarks.part(40))
-
           
-            width = 5
-            #cv2.rectangle(frame, (landmarks.part(36).x - width, center_top[1] - width), (landmarks.part(39).x + width , center_bottom[1] + width),(0,225,255),1)
-            eye = frame[center_top[1]-width: center_bottom[1] + width, landmarks.part(36).x - width : landmarks.part(39).x + width]
+            width = 5 # padding width of eye frame
+            eye = frame[center_top[1] - width: center_bottom[1] + width, landmarks.part(36).x - width : landmarks.part(39).x + width] # frame containing left eye
             
             threshold = r = cv2.getTrackbarPos('Left threshold', 'frame')
             keypoints = blob_process(eye, threshold, "l" )
         
-            if len(keypoints) >=1:
-                frame = cv2.circle(frame, (int(keypoints[0].pt[0] + landmarks.part(36).x - width ), center_top[1]-width+ int(keypoints[0].pt[1])), 1, (0, 255, 255), 5 )
-  
+            
+            if len(keypoints) >= 1 :
+                oldcoordleft =  (int(keypoints[0].pt[0] + landmarks.part(36).x - width ), center_top[1]-width+ int(keypoints[0].pt[1]))
+                frame = cv2.circle(frame, oldcoordleft, 1, (0, 255, 255), 5 )
+            else:
+                if oldcoordleft != False:
+                    frame = cv2.circle(frame, oldcoordleft, 1, (0, 255, 255), 5 )
+
+
+            # Draw dots showing corners of left eye
             left_dot  = cv2.circle(frame, (landmarks.part(36).x, landmarks.part(36).y), 1, (0, 0, 255), -1 )
             right_dot = cv2.circle(frame,(landmarks.part(39).x, landmarks.part(39).y), 1, (0, 0, 255), -1 )
 
-            # right eye on screen
+            # Process right eye
             left_point = (landmarks.part(42).x, landmarks.part(42).y)
             right_point = (landmarks.part(45).x, landmarks.part(45).y)
 
             center_top = midpoint(landmarks.part(43), landmarks.part(44))
             center_bottom = midpoint(landmarks.part(47), landmarks.part(46))
 
-           
-
-            width = 5
-            #cv2.rectangle(frame, (left_point[0] - width, center_top[1] - width), (right_point[0] + width , center_bottom[1] + width),(0,225,255),1)
             eye = frame[center_top[1] - width : center_bottom[1] + width, left_point[0] - width : right_point[0] + width]
+
             threshold = r = cv2.getTrackbarPos('Right threshold', 'frame')
             keypoints = blob_process(eye, threshold, "r" )
         
-            if len(keypoints) >=1:
-                frame = cv2.circle(frame, (int(keypoints[0].pt[0] + left_point[0] - width ), center_top[1] - width + int(keypoints[0].pt[1])), 1, (0, 255, 255), 5 )
-            
+            if len(keypoints) >= 1 :
+                oldcoordright = (int(keypoints[0].pt[0] + left_point[0] - width ), center_top[1] - width + int(keypoints[0].pt[1]))
+                frame = cv2.circle(frame, oldcoordright, 1, (0, 255, 255), 5 )
+            else:
+                if oldcoordright != False:
+                    frame = cv2.circle(frame, oldcoordright, 1, (0, 255, 255), 5 )
+
+            # Draw dots showing corners of right eye
             left_dot  = cv2.circle(frame, (landmarks.part(42).x, landmarks.part(42).y), 1, (0, 0, 255), -1 )
             right_dot = cv2.circle(frame,(landmarks.part(45).x, landmarks.part(45).y), 1, (0, 0, 255), -1 )
         
+
+        now = time.time()
+
+        if now - start > 2:
+            if side == True:
+                dotx = random.randint(0, 100) 
+                side = False
+            else:
+                dotx = random.randint(400, 500) 
+                side = True
+            doty = random.randint(0, 500) 
+            start = time.time()
+
+        frame  = cv2.flip(frame, 1)
+
+        dot = cv2.circle(frame, (dotx, doty), 5, (0, 255, 0), 5 )
+        
         cv2.imshow("frame", frame)
+        
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
-
-
+        
     cap.release()
     cv2.destroyAllWindows()
 
